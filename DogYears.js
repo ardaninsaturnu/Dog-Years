f5e884a3d4bc24b5c208c2dfc560380e8e9e297d
ardaninsaturnu <hsmclk2505@gmail.com>
2020-03-14 12:58:57 +0300

    first commit

---
+++ DogYears.js
@@ -0,0 +1,20 @@
+const myAge =37; // this is my age and it is constant
+
+let earlyYears = 2 ;
+
+earlyYears *= 10.5;
+
+let laterYears = myAge-2 ;
+
+laterYears *= 4;
+
+console.log(earlyYears)
+console.log(laterYears)
+
+let myAgeInDogYears = earlyYears+laterYears
+
+const myName = 'MEHMET ARDA'
+
+console.log(myName.toLowerCase())
+
+console.log(`My name is ${myName.toLowerCase()}.I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
